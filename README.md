9c9e87c27b5d7d611590a1d616d0eea0fc4a2df4
shahajalalweb <shahajalalbadsha5@gmail.com>
2022-10-08 09:51:48 +0600

    border laoding animation

---
+++ README.md
@@ -0,0 +1,1 @@
+# border-loading-animation
---
+++ javascript.js
@@ -0,0 +1,13 @@
+ const container = document.querySelector(".container"),
+ percent = document.querySelector("#percent");
+
+ let perVal = 0;
+
+ let increment = setInterval(() => {
+    perVal++;
+    percent.textContent = `${perVal}%`;
+    if (perVal == 100) {
+        clearInterval(increment);
+        container.classList.remove("active");
+    }
+ }, 100);
